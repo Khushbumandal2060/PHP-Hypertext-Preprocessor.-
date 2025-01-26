7a76a93050e74ced0f55ea50c55f20781bacc715
khushi <khusbumandal2060@gmail.com>
2025-01-26 14:31:47 +0545

    Datatypes

---
+++ datatype.php
@@ -0,0 +1,19 @@
+<?php
+$name="Khushi Mandal";
+//echo var_dump($name);
+
+$num = 100;
+//echo var_dump($num);
+
+$numFloat = 10.10;
+//echo var_dump($numFloat);
+
+$bool=true;
+//echo var_dump($bool);
+
+$arr=["nikhil","golu","Jyota"];
+//echo var_dump($arr);
+
+$empty=null;
+echo var_dump($empty);
+?>
