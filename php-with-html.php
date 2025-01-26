e6e4aaea193176e55a1c012d9e047f7b193655c1
khushi <khusbumandal2060@gmail.com>
2025-01-26 13:54:14 +0545

    Php with Html

---
+++ php-with-html.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1 style='color:red'>php with html</h1>";
+echo "<h3 style='color:blue'>php with html</h3>";
+
+$name="Khushi";
+echo "<h1 style='color:yellow'>$name</h1>";
+echo "<h1 style='color:green'>My name is ".$name."</h1>";
+?>
