60011189c7ace71e38c8b332e3083f03518e6bfb
khushi <khusbumandal2060@gmail.com>
2025-01-28 14:13:44 +0545

    Break and continue in php

---
+++ break-and-continue.php
@@ -0,0 +1,24 @@
+<?php 
+
+$user_need=5;
+/*for($i=0;$i<=10;$i++){
+    echo $i;
+    echo "<br/>";
+    if($i==$user_need){
+        break;
+    }
+} */
+
+for($i=0;$i<=10;$i++){
+    if($i==3 || $i=7){
+        continue;
+    }
+    echo $i;
+    echo "<br/>";
+    
+}
+
+
+
+
+?>
