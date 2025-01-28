a788c9bab1b60eb9eb3ab7bece4455384a37de63
khushi <khusbumandal2060@gmail.com>
2025-01-28 13:59:02 +0545

    Foor loop

---
+++ for_loop.php
@@ -0,0 +1,15 @@
+<?php 
+$table=5;
+//for($start=0;$start<=10;$start++){
+   // echo "Code step<br/>";
+   //echo $start;
+   //echo "<br>";
+
+   for($i=1;$i<=10;$i++){
+   echo $table*$i;
+   echo "<br>";
+}
+
+
+
+?>
