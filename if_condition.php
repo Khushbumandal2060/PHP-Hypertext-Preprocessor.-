539872d918f44c52b4ebd86346e10f64cf695c93
khushi <khusbumandal2060@gmail.com>
2025-01-28 11:44:07 +0545

    if_condition

---
+++ if_condition.php
@@ -0,0 +1,18 @@
+<?php
+$age=10;
+$name="Nikhil Singh";
+
+if($age<18){
+    echo "child";
+}
+
+echo "<br>";
+if($name=="Nikhil Singh"){
+    echo "<h1>Welcome, Nikhil Singh</h1>";
+}
+
+//echo "<br/>";
+//echo "Other";
+
+
+?>
