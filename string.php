ee14671fc4e3fdb73f6f69d24d78e235d419cb6c
khushi <khusbumandal2060@gmail.com>
2025-01-28 11:28:12 +0545

    String

---
+++ string.php
@@ -0,0 +1,11 @@
+<?php
+$name="Khushi";
+$age=21;
+//echo "My name is ".$name;
+
+//echo 'My name is '.$name. ' and my age is'.$age;
+$str="Hello how are you";
+$str1="My name is Khushi Mandal";
+$str.=$str1;
+echo $str;
+?>
