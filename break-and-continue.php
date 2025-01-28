dde0f2e1d74928f35b0e7b41300ed949da5c7b59
khushi <khusbumandal2060@gmail.com>
2025-01-28 14:17:50 +0545

    Fixed continue loop

--- break-and-continue.php
+++ break-and-continue.php
@@ -1,24 +1,20 @@
 <?php 
 
-$user_need=5;
-/*for($i=0;$i<=10;$i++){
+$user_need = 5;
+/* for ($i = 0; $i <= 10; $i++) {
     echo $i;
     echo "<br/>";
-    if($i==$user_need){
+    if ($i == $user_need) {
         break;
     }
 } */
 
-for($i=0;$i<=10;$i++){
-    if($i==3 || $i=7){
+for ($i = 0; $i <= 10; $i++) {
+    if ($i == 3 || $i == 7) { // Fixed the comparison operator
         continue;
     }
     echo $i;
     echo "<br/>";
-    
 }
-
-
-
 
 ?>
