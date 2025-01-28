83721731b144a9eaeacd6242f856e000fcb07269
khushi <khusbumandal2060@gmail.com>
2025-01-28 12:03:15 +0545

    elseif condition

---
+++ else_if_condition.php
@@ -0,0 +1,12 @@
+<?php 
+$country = "India";
+if ($country == "Nepal") {
+    echo "SAARC country";
+} elseif ($country == "France") {
+    echo "European country";
+} elseif ($country == "Canada") {
+    echo "North American country";
+} else {
+    echo "other";
+}
+?>
