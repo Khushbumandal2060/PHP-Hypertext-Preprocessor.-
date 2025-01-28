786669fce0369a10da63d5f183795af9cb8270a3
khushi <khusbumandal2060@gmail.com>
2025-01-28 14:00:20 +0545

    Switch Statement in php

---
+++ switch_statement.php
@@ -0,0 +1,20 @@
+<?php 
+$favouriteColor="blue";
+
+switch($favouriteColor){
+    case "blue":
+    echo " blue color is my favourite color";
+    break;
+    case "red":
+    echo " red color is my favourite color";
+    break;
+    case "yellow":
+    echo " yellow color is my favourite color";
+    break;
+    default:
+    echo "this color is not matched";
+    break;
+
+
+    }
+?>
