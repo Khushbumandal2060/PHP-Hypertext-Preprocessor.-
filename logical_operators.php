b00f92cb80075f5e215ff65db3d63fe57d162bc4
khushi <khusbumandal2060@gmail.com>
2025-01-28 13:48:45 +0545

    Logical operatos

---
+++ logical_operators.php
@@ -0,0 +1,29 @@
+<?php 
+$a=10;
+$b=20;
+//$c=30;
+
+//AND operator
+//if($a==10 && $b==20){
+//  echo "Var a has 10 value";
+//}
+
+//OR operator
+//if($a==10 || $b==20){
+//    echo "condition is true";
+//}
+
+//XOR operator
+//if($a==50 xor $b==20){
+ //   echo "condition is true";
+//}
+
+
+//NOT operator
+if($a!=10){
+    echo "condition is true";
+}
+
+
+
+?>
