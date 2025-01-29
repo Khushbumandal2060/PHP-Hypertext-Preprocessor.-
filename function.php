6998abaaef693a1b822c756fa3049c2f8a9fb62f
khushi <khusbumandal2060@gmail.com>
2025-01-29 12:03:26 +0545

    Function!

---
+++ function.php
@@ -0,0 +1,16 @@
+<?php 
+
+function displayUserDetails(){
+    echo "User name is khushi";
+    echo "<br/>";
+    echo "User age is 21";
+    echo "<br/>";
+    echo "User email id is khushi@123.com";
+    echo "<hr/>";
+}
+
+displayUserDetails();
+
+
+
+?>
