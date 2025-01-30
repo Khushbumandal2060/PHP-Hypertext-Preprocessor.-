66ed7a765bde6735d0caf2f06c1e322d85a52923
khushi <khusbumandal2060@gmail.com>
2025-01-30 13:12:10 +0545

    Array

---
+++ array.php
@@ -0,0 +1,18 @@
+<?php 
+/* 
+$User1 = "Nikhil";     // In php array is a collection od data(no need to be similar data types). 
+$User2 = "Khushi";
+$User3 = "Gola";  
+
+echo $User1;
+*/
+
+$users=["Nikhil","Khushi","Gola","Jyoti"];
+echo $users[0];
+echo "<br>";
+echo $users[1];
+echo "<br/>";
+echo $users[2];
+echo "<br/>";
+echo $users[3];
+?>
