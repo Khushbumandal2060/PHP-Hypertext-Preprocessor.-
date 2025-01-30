2ba3e892cf26ad42d0b996c8f92a41fa690a97ed
khushi <khusbumandal2060@gmail.com>
2025-01-30 13:10:48 +0545

    Some important functions in php!

---
+++ important-function.php
@@ -0,0 +1,26 @@
+<?php 
+
+//1st function
+//$str="Khushi Mandal";
+//echo strlen($str);         //returns the length of the string
+
+//2nd function
+//$var=true;
+//var_dump($var);
+
+//3rd function
+//echo date("d");          // print date
+//echo date("l");          // print day
+//echo date("m");          // print month
+//echo date("m.d.y");      //print month,date and year
+ 
+//4th function
+//echo phpinfo();
+
+//5th function
+$var = "abc";
+echo is_string($var);
+
+
+
+?>
