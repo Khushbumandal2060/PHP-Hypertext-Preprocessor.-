06b9c0e4640d3f3cd713e3d93ef6d9c34772b578
khushi <khusbumandal2060@gmail.com>
2025-01-31 08:32:08 +0545

    Associative array is an array that uses named keys instead of numeric indexes.

---
+++ associative-array.php
@@ -0,0 +1,21 @@
+<?php 
+$userDetails =[
+    "name"=>"khushi",
+    "age"=>21,
+    "city"=>"Kathmandu",
+    "email"=>"khush123@gmail.com",
+    "state"=>2
+];
+
+echo $userDetails["name"];
+echo "<br/>";
+echo $userDetails["age"];
+echo "<br/>";
+echo $userDetails["city"];
+echo "<br/>";
+echo $userDetails["email"];
+echo "<br/>";
+echo $userDetails["state"];
+
+
+?>
