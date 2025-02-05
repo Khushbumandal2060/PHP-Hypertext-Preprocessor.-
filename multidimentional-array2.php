d0b6e2105277ec966c18dc13ecf937222e3e52b5
khushi <khusbumandal2060@gmail.com>
2025-02-05 11:46:08 +0545

    Multidimentional Array

---
+++ multidimentional-array2.php
@@ -0,0 +1,13 @@
+<?php 
+$users=[
+    [1,"Nikhil","Rautahat","singhsansar@Gmail.com"],
+    [2,"jyoti","Sisautiya","jyotimandal144@gmail.com"],
+    [3,"Golu","Malangwa","glumandal322@gmail.com"]
+];
+
+
+print_r($users);
+
+
+
+?>
