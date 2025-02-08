7aa42d76d669968c9c2814a24dc0ca43331e198c
khushi <khusbumandal2060@gmail.com>
2025-02-08 13:34:33 +0545

    Cookies in Php

--- cookies.php
+++ cookies.php
@@ -2,7 +2,7 @@
 setcookie("fruit","apple",time()+(86400));
 
 if(isset($_COOKIE['fruit'])){
-    print_r($_COOKIE);
+    echo "current cookie is ". $_COOKIE['fruit'];
 }else{
     echo "no cookie set";
 }
