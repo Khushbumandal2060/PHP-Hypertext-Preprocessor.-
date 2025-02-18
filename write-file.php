11445efba731c8a98aedd1d6a5129511fd3673b5
khushi <khusbumandal2060@gmail.com>
2025-02-18 11:52:43 +0545

    Make file with form data

--- write-file.php
+++ write-file.php
@@ -1,10 +1,24 @@
 <?php 
-$fileName = "dummy.txt"; 
-$content = "files/this is a dummy file"; // Corrected semicolon
+if(isset($_POST['filename'])){
+    $fileName = "files/".$_POST['filename']; 
+    //$content =  "files/this is a dummy file"; 
+    $content=$_POST['filename'];
+    $file = fopen($fileName, "w") or die("Unable to create file"); 
+     fwrite($file, $content); 
+     fclose($file);
+     echo "File created successfully"; 
+}
 
-$file = fopen($fileName, "w") or die("Unable to create file"); 
-fwrite($file, $content); 
-fclose($file);
-
-echo "File created successfully"; 
 ?>
+
+<form action="" method="post">
+    <input type="text" placeholder="enter file name" name="filename" />
+    <br />
+    <br />
+    <textarea name="content">
+
+    </textarea>
+    <br />
+    <br />
+    <button>Create File</button>
+</form>
