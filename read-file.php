113eb9329feec1f18d53d7c1d8611aeea19c4808
khushi <khusbumandal2060@gmail.com>
2025-02-18 11:59:46 +0545

    Read file

--- read-file.php
+++ read-file.php
@@ -4,3 +4,10 @@ $myFile= fopen($file,"r") or die("unable to read file");
 echo fread($myFile,filesize($file));
 fclose($myFile);
 ?>
+
+<form action="" method="post">
+    <input type="file" name="file" />
+    <br/>
+    <br/>
+    <button>Read File</button>
+</form>
